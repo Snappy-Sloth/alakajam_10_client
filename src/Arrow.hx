2f853f880446c706b3aac77c85e28d65223a037e
Titaninette <letoffe.laetitia@gmail.com>
2020-09-19 12:09:46 +0200

    Added interactive arrows

--- src/Arrow.hx
+++ src/Arrow.hx
@@ -1,6 +1,11 @@
+import h2d.Interactive;
+import h2d.Graphics;
+
 class Arrow extends h2d.Layers {
     public var cx : Int;
-	public var cy : Int;
+    public var cy : Int;
+    
+    var inter : Interactive;
 
     public function new(tx:Int, ty:Int) {
         super();
@@ -11,5 +16,12 @@ class Arrow extends h2d.Layers {
 		arrow.beginFill(0xff00ff);
         arrow.drawRect(0, 0, Const.ARROW_TILE_WIDTH, Const.ARROW_TILE_HEIGHT);
         this.setPosition(cx*Const.MAP_TILE_SIZE-Const.ARROW_TILE_WIDTH/2, cy*Const.MAP_TILE_SIZE+Const.ARROW_TILE_HEIGHT/2);
+
+        /*inter = new Interactive(Const.ARROW_TILE_WIDTH, Const.ARROW_TILE_HEIGHT, this);
+        inter.backgroundColor = 0x55ff00ff;
+
+        inter.onClick = function(e) {
+
+        }*/
     }
 }
