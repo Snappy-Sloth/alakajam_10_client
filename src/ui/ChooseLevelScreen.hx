878e53a57e15823c54426b8c5127c41e68122c76
Titaninette <letoffe.laetitia@gmail.com>
2020-09-20 14:10:30 +0200

    chooselevel modify

--- src/ui/ChooseLevelScreen.hx
+++ src/ui/ChooseLevelScreen.hx
@@ -6,6 +6,8 @@ class ChooseLevelScreen extends dn.Process {
 
 	var flow : h2d.Flow;
 
+	var levels : Array<Data.Campaign> = [];
+
 	public function new() {
 		super(Main.ME);
 
@@ -13,11 +15,17 @@ class ChooseLevelScreen extends dn.Process {
 
 		ME = this;
 
+		levels = [];
+
+		for (lvl in Data.Campaign.all) {
+			levels.push(lvl);
+		}
+
 		flow = new h2d.Flow(root);
 		flow.layout = Vertical;
         flow.verticalSpacing = 20;
         
-        var level1Btn = new Button("Level 1", Main.ME.showDebugLevel2x2);
+        /*var level1Btn = new Button("Level 1", Main.ME.showDebugLevel2x2);
 		flow.addChild(level1Btn);
 
 		var level2Btn = new Button("Level 2", Main.ME.showDebugLevel2x3);
@@ -27,7 +35,7 @@ class ChooseLevelScreen extends dn.Process {
 		flow.addChild(level3Btn);
 
 		var level4Btn = new Button("Level 4", Main.ME.showDebugLevel3x3);
-		flow.addChild(level4Btn);
+		flow.addChild(level4Btn);*/
 
 		onResize();
 	}
