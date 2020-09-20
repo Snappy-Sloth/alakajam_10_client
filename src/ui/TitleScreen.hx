1f44070f62fdf70f6cda0ea1bfa3c4ebd8046f07
Tipyx <mathieu.pistol@gmail.com>
2020-09-20 19:52:26 +0200

    Added Title

--- src/ui/TitleScreen.hx
+++ src/ui/TitleScreen.hx
@@ -15,7 +15,13 @@ class TitleScreen extends dn.Process {
 
 		flow = new h2d.Flow(root);
 		flow.layout = Vertical;
+		flow.horizontalAlign = Middle;
 		flow.verticalSpacing = 20;
+
+		var title = Assets.tiles.h_get("title");
+		flow.addChild(title);
+
+		flow.addSpacing(50);
 
 		var campaignBtn = new Button("Campaign", Main.ME.startCampaign);
 		flow.addChild(campaignBtn);
