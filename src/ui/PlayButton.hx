f8df15b778325832ad81cac43d24b4b493348405
Tipyx <mathieu.pistol@gmail.com>
2020-10-09 11:21:10 +0200

    Replace play assets

--- src/ui/PlayButton.hx
+++ src/ui/PlayButton.hx
@@ -18,17 +18,27 @@ class PlayButton extends h2d.Object {
 
 		this.level = level;
 
-		spr = Assets.tiles.h_get("play", this);
+		spr = Assets.tiles.h_get("playButtonIdle", this);
 		var inter = new h2d.Interactive(spr.tile.width, spr.tile.height, this);
 
 		inter.onClick = (e)->onClick();
+
+		inter.onRelease = inter.onOver = function (e) {
+			spr.set(currentStatus == Play ? "playButtonOver" : "restartButtonOver");
+		}
+		inter.onReleaseOutside = inter.onOut = function (e) {
+			spr.set(currentStatus == Play ? "playButtonIdle" : "restartButtonIdle");
+		}
+		inter.onPush = function (e) {
+			spr.set(currentStatus == Play ? "playButtonPress" : "restartButtonPress");
+		}
 	}
 
 	public function onClick() {
 		switch (currentStatus) {
 			case Play:
 				currentStatus = Pause;
-				spr.set("restartBtn");
+				spr.set("restartButtonIdle");
 				level.playBtnPressed();
 				case Pause:
 				level.resetShips();
@@ -37,7 +47,7 @@ class PlayButton extends h2d.Object {
 
 	public function reset() {
 		currentStatus = Play;
-		spr.set("play");
+		spr.set("playButtonIdle");
 	}
 
 }
